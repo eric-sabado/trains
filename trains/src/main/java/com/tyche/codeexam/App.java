b44edb5440919389f31d4bd7dbae0858f369d24e
Eric Sabado <eric_sabado@yahoo.com>
2015-02-03 10:43:43 +0800

    Added initial model for Vertex
    Signed-off-by: Eric Sabado <eric_sabado@yahoo.com>

--- trains/src/main/java/com/tyche/codeexam/App.java
+++ trains/src/main/java/com/tyche/codeexam/App.java
@@ -1,5 +1,8 @@
 package com.tyche.codeexam;
 
+import com.tyche.codeexam.model.Vertex;
+
+
 /**
  * Hello world!
  *
@@ -9,5 +12,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Vertex vertex = new Vertex("Quezon City");
+        
+        System.out.println(vertex.getName());
+        
     }
 }
---
+++ trains/src/main/java/com/tyche/codeexam/model/Vertex.java
@@ -0,0 +1,21 @@
+package com.tyche.codeexam.model;
+
+/**
+ * @author Eric
+ *
+ */
+public class Vertex {
+	private String name;
+
+	public Vertex(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
