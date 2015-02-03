b3927bcfee2b84b708873f6b66616078492392dd
Eric Sabado <eric_sabado@yahoo.com>
2015-02-03 19:55:48 +0800

    Implementation of Digraph.computeWeight()
    Signed-off-by: Eric Sabado <eric_sabado@yahoo.com>

--- trains/src/main/java/com/tyche/codeexam/model/Digraph.java
+++ trains/src/main/java/com/tyche/codeexam/model/Digraph.java
@@ -1,24 +1,42 @@
 package com.tyche.codeexam.model;
 
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Digraph {
+	public static final String NO_SUCH_ROUTE = "NO SUCH ROUTE";
+	
 	private Set<Node> nodes;
-	private Set<Edge> edges;
+	private Map<String, Edge> edges;
 
-	Digraph() {
-		this.nodes = new HashSet<>();
-		this.edges = new HashSet<>();
+	public Digraph() {
+		this.nodes = new HashSet<Node>();
+		this.edges = new HashMap<String, Edge>();
 	}
 
 	public void addEdge(Edge edge) {
-		this.edges.add(edge);
-		addNodes(edge);
+		this.edges.put(edge.getName(), edge);
+		registerNodes(edge);
 	}
 
-	private void addNodes(Edge edge) {
+	public Float computeWeight(List<Edge> edgeQueries) throws Exception {
+		Float totalWeight = Float.valueOf(0);
+
+		for (Edge edgeQuery : edgeQueries) {
+			Edge edge = edges.get(edgeQuery.getName());
+			if (null == edge) {
+				throw new Exception(NO_SUCH_ROUTE);
+			}
+
+			totalWeight += edge.getWeight();
+		}
+		return totalWeight;
+	}
+
+	private void registerNodes(Edge edge) {
 		Node head = edge.getHead();
 		Node tail = edge.getTail();
 
@@ -28,15 +46,11 @@ public class Digraph {
 		}
 	}
 
-	public void addEdge(Collection<Edge> edges) {
-		this.edges.addAll(edges);
-	}
-
 	public Set<Node> getNodes() {
 		return nodes;
 	}
 
-	public Set<Edge> getEdges() {
+	public Map<String, Edge> getEdges() {
 		return edges;
 	}
 }
