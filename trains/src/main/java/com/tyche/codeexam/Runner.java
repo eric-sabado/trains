8c2881d0256c52dd0e4dec1338d8763a4085d81a
Eric Sabado <eric_sabado@yahoo.com>
2015-02-04 01:13:18 +0800

    Runner cleanup
    Signed-off-by: Eric Sabado <eric_sabado@yahoo.com>

--- trains/src/main/java/com/tyche/codeexam/Runner.java
+++ trains/src/main/java/com/tyche/codeexam/Runner.java
@@ -1,65 +1,65 @@
 package com.tyche.codeexam;
 
-import java.util.ArrayList;
+import static com.tyche.codeexam.util.CommandStringParser.toEdges;
+import static com.tyche.codeexam.util.CommandStringParser.toNodes;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+import java.util.List;
 import java.util.Scanner;
 
+import com.tyche.codeexam.exception.MissingRouteException;
 import com.tyche.codeexam.model.Digraph;
-import com.tyche.codeexam.model.Edge;
 import com.tyche.codeexam.model.Node;
 
 public class Runner {
 
-	final static Digraph digraph;
-	final static ArrayList<Edge> edgeQuery;
+	private static Digraph digraph;
+	private static List<Node> nodeQuery;
+	private static Scanner scanner;
+	private static String digraphString;
 
 	static {
 		digraph = new Digraph();
-		edgeQuery = new ArrayList<Edge>();
+		scanner = new Scanner(System.in);
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Path Query:\n");
-		
-		Scanner sc = new Scanner(System.in);		
-		String query = sc.next();
-		
-		buildDigrpah();
-		buildEdgeQuery(query);
-		computePath();
-		
-		sc.close();
+		captureDigraphCommand();
+		captureNodeQueryCommand();
 	}
 
-	private static void buildEdgeQuery(String arg) {
-		String[] nodeName = arg.split("");
+	private static void captureDigraphCommand() {
+		System.out.println("Direct Graph Definition: ");
 
-		for (int i = 0; i < nodeName.length; i++) {
-			if (i + 1 < nodeName.length) {
-				edgeQuery.add(new Edge(new Node(nodeName[i]), new Node(
-						nodeName[i + 1]), null));
-			}
+		digraphString = scanner.next();
+		if (isEmpty(digraphString)) {
+			captureDigraphCommand();
+		} else {
+			buildDigraph();
 		}
 	}
 
-	private static void buildDigrpah() {
-		digraph.addEdge(new Edge(new Node("A"), new Node("B"), Float.valueOf(5)));
-		digraph.addEdge(new Edge(new Node("B"), new Node("C"), Float.valueOf(4)));
-		digraph.addEdge(new Edge(new Node("C"), new Node("D"), Float.valueOf(8)));
-		digraph.addEdge(new Edge(new Node("D"), new Node("C"), Float.valueOf(8)));
-		digraph.addEdge(new Edge(new Node("D"), new Node("E"), Float.valueOf(6)));
-		digraph.addEdge(new Edge(new Node("A"), new Node("D"), Float.valueOf(5)));
-		digraph.addEdge(new Edge(new Node("C"), new Node("E"), Float.valueOf(2)));
-		digraph.addEdge(new Edge(new Node("E"), new Node("B"), Float.valueOf(3)));
-		digraph.addEdge(new Edge(new Node("A"), new Node("E"), Float.valueOf(7)));
+	private static void captureNodeQueryCommand() {
+		System.out.println("Route? ");
 
+		buildNodeQuery(scanner.next());
+		computePath();
+		captureNodeQueryCommand();
+	}
+
+	private static void buildNodeQuery(String routeCommandString) {
+		nodeQuery = toNodes(routeCommandString);
 	}
 
 	private static void computePath() {
-
 		try {
-			System.out.println(digraph.computeWeight(edgeQuery));
-		} catch (Exception e) {
+			System.out.println("Distance: " + digraph.computeWeight(nodeQuery));
+		} catch (MissingRouteException e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	private static void buildDigraph() {
+		digraph.addEdge(toEdges(digraphString));
 	}
 }
